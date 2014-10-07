415c0cb866bfc3a749b933f5c383b2c22b7ea954
t3500 <bwtian@gmail.com>
2014-10-07 11:57:40 +0900

    + ‘R/ge.rawBbox.R’

---
+++ R/ge.rawBbox.R
@@ -0,0 +1,11 @@
+ge.rawBbox <- function(xmin,xmax,ymin,ymax,crs = NA){
+        ### Make a bbox rect SPDF from LL and TR point.
+        Polygon  <- sp::Polygon(cbind(c(xmin, xmin, xmax, xmax, xmin),
+                           c(ymin, ymax, ymax, ymin, ymin)))
+        Polygons  <- sp::Polygons(list(Polygon), 1) #ID for row.names = 1
+        SP  <- sp::SpatialPolygons(list(Polygons))
+        data_d  <- data.frame(name = "bbox", row.names= row.names(Polygons))
+        SPDF  <- sp::SpatialPolygonsDataFrame(SP, data = data_d)
+        sp::proj4string(SPDF)  <- crs
+        return(SPDF)
+}
