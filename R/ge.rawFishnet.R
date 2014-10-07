7dc3de3e2b39b23812b71f61686c0a861d642408
t3500 <bwtian@gmail.com>
2014-10-07 11:56:08 +0900

    + ‘R/ge.rawFishnet.R’

---
+++ R/ge.rawFishnet.R
@@ -0,0 +1,9 @@
+ge.rawFishnet  <- function(x1,x2,y1,y2,rx=1000,ry=rx) {
+        x.range.v <- seq(as.numeric(x1),as.numeric(x2),by = rx)
+        y.range.v <- seq(as.numeric(y1),as.numeric(y2),by = ry)
+        grid.m    <- outer(x.range.v, y.range.v, paste, sep = ",")
+        grid.v    <- as.vector(grid.m)
+        grid.d    <- as.data.frame(grid.v)
+        fishnet.d <- data.frame(do.call('rbind', strsplit(as.character(grid.d[,1]), ',', fixed=TRUE)))
+        colnames(fishnet.d)  <- c("x","y")
+}
