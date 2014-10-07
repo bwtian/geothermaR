008b059ec603b4062f0cfbeae0416e09349d481f
t3500 <bwtian@gmail.com>
2014-10-07 17:53:06 +0900

    / ‘demo/ge11_3dgrid.R’

--- demo/ge11_3dgrid.R
+++ demo/ge11_3dgrid.R
@@ -1,14 +1,5 @@
-shp <- maptools::readShapePoly(studyArea.shp)
-proj4string(shp) <- CRS("your courdinate system")
-## Here I took a irregular polygon as example
-P <- sp::Polygon(cbind(c(2500, 2500, 3500, 3500, 2500),
-                       c(1500, 4500, 4500, 5500, 1500)))
-Ps <- sp::Polygons(list(P), "P")
-SP <- sp::SpatialPolygons(list(Ps))
-sp::plot(SP)
-shp <- SP
 ## then sample use sp package to get a 2D grid, and you can choose the type of grid, here is a regular one
-grid2d <- sp::spsample(shp,type = "regular", cellsize = c(100,100), offset =c(0.5, 0.5))
+grid2d <- readRDS("~/Dropbox/2data/hkd//hkdmskbig_grdi2d1h.Rds")
 sp::plot(grid2d)
 ## until now you get xy data,then we create 3d point grid with depth to 100m to 1000m as example
 xy <- grid2d@coords
