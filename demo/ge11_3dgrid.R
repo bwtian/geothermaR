6a6bdd38e301af2a14e5127211c85a309c335d45
t3500 <bwtian@gmail.com>
2014-10-07 17:54:15 +0900

    / ‘demo/ge11_3dgrid.R’

--- demo/ge11_3dgrid.R
+++ demo/ge11_3dgrid.R
@@ -1,7 +1,6 @@
 ## then sample use sp package to get a 2D grid, and you can choose the type of grid, here is a regular one
 grid2d <- readRDS("~/Dropbox/2data/hkd//hkdmskbig_grdi2d1h.Rds")
-sp::plot(grid2d)
-## until now you get xy data,then we create 3d point grid with depth to 100m to 1000m as example
+str(grid2d)
 xy <- grid2d@coords
 xyz <- list()
 for (i in 1:10) {
