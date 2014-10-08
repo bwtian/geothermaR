671af378bcc553ce72d40b32992d26c8c3687431
Bingwei Tian <bwtian@gmail.com>
2014-10-08 12:16:49 +0900

    / ‘R/ge.ggsave.R’

--- R/ge.ggsave.R
+++ R/ge.ggsave.R
@@ -2,9 +2,9 @@ ge.ggsave <- function(name, scale = 1, dpi = 300) {
         ## write data.frame as csv and rds file into data folder using df name itselt
 now <- format(Sys.time(), "_%y%m%d_%H%M%S")
 ## eps/ps, tex (pictex), pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
-pngName  <- paste0(deparse(substitute(name)), now, ".png")
-tifName  <- paste0(deparse(substitute(name)), now, ".tiff")
-pdfName  <- paste0(deparse(substitute(name)), now, ".pdf")
+pngName  <- paste0(deparse(substitute(name)), ".png")
+tifName  <- paste0(deparse(substitute(name)), ".tiff")
+pdfName  <- paste0(deparse(substitute(name)), ".pdf")
 ggsave(pngName,scale = scale, dpi = 300)
 ggsave(tifName,scale = scale, dpi = 300)
 ggsave(pdfName,scale = scale, dpi = 300)
