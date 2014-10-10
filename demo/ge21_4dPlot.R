bd25ffbf530e7998d1641f0faf3185835c712b30
lab1 <bwtian@gmail.com>
2014-10-10 17:35:45 +0900

    + ‘demo/ge21_4dPlot.R’

---
+++ demo/ge21_4dPlot.R
@@ -0,0 +1,13 @@
+source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
+uk  <- readRDS("~/Share500sda/result/uk.df_141010_111357.Rds")
+head(uk)
+c <- cut(uk$Tuk,breaks=255)
+cols <- rainbow(255)[as.numeric(c)]
+
+library(ggplot2)
+library(lattice)
+levelplot(var1.pred ~ x+y | z,uk)
+library(rgl)
+open3d()
+plot3d(uk$x, uk$y, uk$z, col=cols,type="p",size=1)
+surface3d(uk$x, uk$y, uk$z, col=cols,type="p",size=1)
