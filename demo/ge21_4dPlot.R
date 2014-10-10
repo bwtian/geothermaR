7ca581b0ad00f302ec26bf8693096818118d00e9
lab1 <bwtian@gmail.com>
2014-10-10 12:15:09 +0900

    / ‘demo/ge21_4dPlot.R’

--- demo/ge21_4dPlot.R
+++ demo/ge21_4dPlot.R
@@ -5,15 +5,3 @@ c <- cut(uk$Tuk,breaks=255)
 cols <- rainbow(255)[as.numeric(c)]
 open3d()
 plot3d(uk$x, uk$y, uk$z, col=cols,type="s",size=1)
-
-vertices <- c(
-        -1.0, -1.0, 0, 1.0,
-        1.0, -1.0, 0, 1.0,
-        1.0,  1.0, 0, 1.0,
-        -1.0,  1.0, 0, 1.0
-)
-head(vertices)
-indices <- c( 1, 2, 3, 4 )
-
-open3d()
-wire3d( qmesh3d(vertices,indices) )
