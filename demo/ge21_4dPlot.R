388046ef33000b89de630db945733f8f0c217910
lab1 <bwtian@gmail.com>
2014-10-10 12:17:09 +0900

    / ‘demo/ge21_4dPlot.R’

--- demo/ge21_4dPlot.R
+++ demo/ge21_4dPlot.R
@@ -1,7 +1,11 @@
 source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
 uk  <- readRDS("~/Share500sda/result/uk.df_141010_111357.Rds")
-library(rgl)
+
 c <- cut(uk$Tuk,breaks=255)
 cols <- rainbow(255)[as.numeric(c)]
+
+
+library(rgl)
 open3d()
-plot3d(uk$x, uk$y, uk$z, col=cols,type="s",size=1)
+plot3d(uk$x, uk$y, uk$z, col=cols,type="p",size=1)
+surface3d(uk$x, uk$y, uk$z, col=cols,type="p",size=1)
