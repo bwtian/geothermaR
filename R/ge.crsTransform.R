db514294ac73e316f3bf7b35bdc87ce012c19013
t3500 <bwtian@gmail.com>
2014-10-15 15:10:43 +0900

    / ‘R/ge.crsTransform.R’

--- R/ge.crsTransform.R
+++ R/ge.crsTransform.R
@@ -1,14 +1,14 @@
 ge.crsTransform  <- function(df, x, y, xName, yName, fromCRS, toCRS) {
       ### 140510 transfer CRS in a dataframe fromat, x, y will be repaced in data.frame
-       df$x  <- df[,which(colnames(df) == as.character(substitute(x)))]
-       df$y  <- df[,which(colnames(df) == as.character(substitute(y)))]
-      library(sp)
-      library(rgdal)
-      coordinates(df)  <- c("x", "y")
-      proj4string(df)  <- CRS(fromCRS)
-      df <- spTransform(df,CRS(toCRS))
-      df <- as.data.frame(df)
-      names(df)[which(names(df) == "x")] <- as.character(substitute(xName))
-      names(df)[which(names(df) == "y")] <- as.character(substitute(yName))
-      return(df)
-    }
+    df$x  <- as.numeric(df[,which(colnames(df) == as.character(substitute(x)))])
+    df$y  <- as.numeric(df[,which(colnames(df) == as.character(substitute(y)))])
+    require(sp)
+    require(rgdal)
+    coordinates(df)  <- c("x", "y")
+    proj4string(df)  <- CRS(fromCRS)
+    df <- spTransform(df,CRS(toCRS))
+    df <- as.data.frame(df)
+    names(df)[which(names(df) == "x")] <- as.character(substitute(xName))
+    names(df)[which(names(df) == "y")] <- as.character(substitute(yName))
+    return(df)
+}
