19612bbd2414626120a8c8de7bc02e611c5ad857
t3500 <bwtian@gmail.com>
2014-10-21 12:01:59 +0900

    / ‘R/ge.xy2bbox.R’

--- R/ge.xy2bbox.R
+++ R/ge.xy2bbox.R
@@ -1,5 +1,9 @@
 ge.xy2bboxSPDF <- function(xmin,xmax,ymin,ymax,crs = NA){
         ### Make a bbox rect SPDF from LL and TR point.
+        xmin  <- as.numeric(xmin)
+        xmax <- as.numeric(xmax)
+        ymin  <- as.numeric(ymin)
+        ymax  <- as.numeric(ymax)
         Polygon  <- sp::Polygon(cbind(c(xmin, xmin, xmax, xmax, xmin),
                            c(ymin, ymax, ymax, ymin, ymin)))
         Polygons  <- sp::Polygons(list(Polygon), 1) #ID for row.names = 1
