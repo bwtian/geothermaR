11e295b42aab510dd08bfaf36d8fe179b7932799
tian <bwtian@gmail.com>
2014-11-28 00:46:39 +0900

    / ‘R/ge.ggsave.R’

--- R/ge.ggsave.R
+++ R/ge.ggsave.R
@@ -1,4 +1,4 @@
-ge.ggsave <- function(name, scale = 1, dpi = 300) {
+ge.ggsave <- function(name, scale = 1, dpi = 300, ...) {
 now <- format(Sys.time(), "_%y%m%d_%H%M%S")
 ## eps/ps, tex (pictex), pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
 #pngName  <- paste0(deparse(substitute(name)), now, ".png")
