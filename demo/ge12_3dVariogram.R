631e048f73519a7adff5c8b61e9c543b6f695948
tian <bwtian@gmail.com>
2014-12-01 00:22:31 +0900

    / ‘demo/ge12_3dVariogram.R’

--- demo/ge12_3dVariogram.R
+++ demo/ge12_3dVariogram.R
@@ -1,23 +1,25 @@
 source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
 # load data and grid
 hkdbhs  <- readRDS("~/Dropbox//2data//dataProduct//hkd//hkd_profiles_140806_164333.Rds")
-hkd3dgrid  <- readRDS("~/Dropbox/2data/hkd/hkf1k3d1h-1k_df.Rds")
+hkd3dgrid  <- readRDS("~/Dropbox/2data/hkd/hkd1k3d15h.df.Rds")
 
 ## simple
 
 hkdbh  <- hkdbhs[hkdbhs$Depths >=100 & hkdbhs$Depths <=1500,]
 ### Normality check
 hist(log(hkdbh$Temperature))
+qqnorm(log(hkdbh$Temperature))
+qqline(log(hkdbh$Temperature), col = "red")
 x  <- hkdbh$x_lccwgs84
 y  <- hkdbh$y_lccwgs84
 z  <- hkdbh$Depths
 t  <- hkdbh$Temperature
-Tlog  <- log10(hkdbh$Temperature)
+Tlog  <- log(hkdbh$Temperature)
 xyzv  <- as.data.frame(cbind(x,y,z,t,Tlog))
-### Trend
+###  Trend Surface Analysis
 plot(xyzv)
-pairs(xyzv)
-class(xyzv)
+# pairs(xyzv)
+# class(xyzv)
 df  <- na.omit(xyzv)
 ### remove duplicated spatial point
 summary(df)
@@ -33,29 +35,50 @@ grid  <- hkd3dgrid
 coordinates(grid) <- ~x+y+z
 proj4string(grid)  <- CRS(lccWgs84)
 gridded(grid) <- TRUE
+
+Tlog.trend  <-  krige(Tlog ~ x + y + z , spdf, grid)
+Tlog.trend.df  <- as.data.frame(Tlog.trend)
+levelplot(data = Tlog.trend.df, exp(var1.pred) ~ x*y| z,  contour=TRUE)
+#rgl::surface3d(Tlog.trend.df$x, Tlog.trend.df$y, Tlog.trend.df$z, col = rainbow(10))
+###
+Tlog.trend.lm <- lm(Tlog ~   z, spdf)
+summary(Tlog.trend.lm)
+
+plot(Tlog.trend.lm)
+
+
+
 head(grid)
 ### log plot
-
-
-
-vgmTlog <- variogram(Tlog ~ 1, spdf, cutoff = 2500)
+vgmTlog <- variogram(Tlog ~ 1, spdf, cutoff = 100000)
+vgmTlog 
+vgmTlog <- variogram(Tlog ~ 1, spdf,
+                     boundaries = c(seq(100,1000,100), seq(2000,10000,1000)))
+vgmTlog <- variogram(Tlog ~ z, spdf,
+                     boundaries = c(seq(100,1000,100), seq(2000,45000,5000)))
+plot(vgmTlog)
 ## Universal Kriging
-vgmTlog <- variogram(Tlog ~ z, spdf, cutoff = 2500)
+#vgmTlog <- variogram(Tlog ~ z^3, spdf, cutoff = 50000,cloud = T)
 #vgmTlog <- variogram(logT ~ z, spdf, cutoff = 2500, alpha = c(0,45,90,135), beta = c(0,45,90,135))
 # Trend
 #trend  <- gstat(NULL, "trend", logT ~ z^2, data = spdf)
 #vgmTlog  <- variogram(trend, cutoff = 2500)
-vgmTlog
-plot(vgmTlog)
+show.vgms()
 
 
+v.eye  <- vgm(0.28, "Pen", 35000, 0)
+v.eye1  <- vgm(psill = 0.16,  model = "Gau",  range=700,  nugget=0)
+v.eye   <- vgm(psill = 0.13,  model = "Sph",  range=35000,  nugget=0,  add.to=v.eye1)
+v.eye
+plot(vgmTlog, v.eye)
 
-vgmGauTlog <- fit.variogram(vgmTlog, vgm(0.08, "Gau", 2500, 0))
+vgmGauTlog <- fit.variogram(vgmTlog, v.eye)
 vgmGauTlog
 plot(vgmTlog, vgmGauTlog)
+plot(vgmTlog, v.eye)
 attr(vgmGauTlog, "SSErr")
 grid$TlogUK <- krige(Tlog~z, spdf, grid, model = vgmGauTlog)
-
+show.vgms
 
 ### post-krige
 log(exp(3))
