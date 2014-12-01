f8852e572fe678109a51f439ac15c22ff8d5f4aa
tian <bwtian@gmail.com>
2014-12-01 16:41:01 +0900

    / ‘demo/ge12_3dUKVariogram.R’

--- demo/ge12_3dUKVariogram.R
+++ demo/ge12_3dUKVariogram.R
@@ -10,12 +10,23 @@ plot(vgmTlog)
 v.eye1  <- vgm(psill = 0.16,  model = "Gau",  range=700,  nugget=0)
 v.eye   <- vgm(psill = 0.13,  model = "Sph",  range=35000,  nugget=0,  add.to=v.eye1)
 v.eye
-plot(vgmTlog, v.eye)
+summary(v.eye)
+plot(vgmTlog, v.eye, color = "blue", 
+     xlab = 'Distance [m]', 
+     ylab = expression("Semivariance [(ln"~degree*C~")"^2~"]"),
+     )
 
+vgm  <- v.eye
 ge.ggVariogram <- function(vgm){
-        ggplot(vgm, aes(x=dist, y=gamma)) + geom_point(color='blue') + geom_text(aes(label=np))
-        + geom_smooth(fill='white', alpha=0) + 
+        ggplot(vgm, aes(x=dist, y=gamma)) + 
+          geom_point(color='blue')
+        + 
+          geom_text(aes(label=np)) +
+        geom_smooth(fill='white', alpha=0) + 
         geom_ribbon(aes(ymin=gamma -2*sd,ymax=gamma+2*sd),alpha=0.2) +
         xlab('Distance') + ylab('Semivariogram') + 
-        + theme_bw()
+        theme_bw()
+}
+ge.ggVariogram(v.eye)
+
 grid$TlogUK.eye <- krige(Tlog~z, spdf, grid, model = v.eye)
