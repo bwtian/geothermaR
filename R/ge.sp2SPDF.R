9f2351e9a527a96401ff9fabcd096ead0cd44cb9
lab1 <bwtian@gmail.com>
2014-12-01 11:17:33 +0900

    / ‘R/ge.sp2SPDF.R’

--- R/ge.sp2SPDF.R
+++ R/ge.sp2SPDF.R
@@ -1,8 +1,5 @@
-ge.sp2SPDF  <- function(sp) {
-        df  <- data.frame(as.factor(1:length(sp)))
-        spdf <- sp::SpatialPointsDataFrame(sp, df)
-        grd  <- spdf
-        sp::gridded(grd)  <- TRUE
-        grd_SPDF  <- as(grd, "SpatialPolygonsDataFrame")
-        return(grd_SPDF)
+ge.sp2spdf  <- function(sp){
+  sp  <- as(grid, "SpatialPoints")
+  df  <- data.frame(as.factor(1:length(sp)))
+  spdf <- sp::SpatialPointsDataFrame(sp, df)
 }
