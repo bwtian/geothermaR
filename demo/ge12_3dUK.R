64ba2854221c968758e985f7889b313460e4b668
lab1 <bwtian@gmail.com>
2014-12-04 18:37:46 +0900

    / ‘demo/ge12_3dUK.R’

--- demo/ge12_3dUK.R
+++ demo/ge12_3dUK.R
@@ -2,6 +2,7 @@ source("~/SparkleShare/Rprofile/R/Rsettings/phdRsettings.R")
 # load data and grid
 hkdbhs  <- readRDS("~/Dropbox//2data//dataProduct//hkd/hkdbh_141201_110703.Rds")
 hkd3dgrid  <- readRDS("~/Dropbox/2data//dataProduct/hkd/hkd15hgrid_141201_111200.Rds")
+summary(hkdbhs)
 spdf  <- hkdbhs
 grid  <- hkd3dgrid
 ### research logt
