206d6a3f58605ae72c03a2480ab6dd050393c740
tian <bwtian@gmail.com>
2015-02-20 12:46:01 +0900

    / ‘demo/jp3Dvariogram.R’

--- demo/jp3Dvariogram.R
+++ demo/jp3Dvariogram.R
@@ -44,13 +44,80 @@ spdf0 <- remove.duplicates(spdf)
 spdf <- spdf0
 zerodist(spdf) ##check again
 vgmTlog <- variogram(tlog ~ z, spdf,
-                     boundaries = c(seq(100,1000,100), seq(5000,50000,5000)))
+                     boundaries = c(seq(100,1000,100), seq(10000,60000,5000)))
 plot(vgmTlog)
 show.vgms()
 v.eye1  <- vgm(psill = 0.155,  model = "Gau",  range=700,  nugget=0)
-v.eye   <- vgm(psill = 0.38,  model = "Exc",  range=35000,  nugget=0,  add.to=v.eye1)
+v.eye   <- vgm(psill = 0.44,  model = "Exc",  range=60000,  nugget=0,  add.to=v.eye1)
 v.eye
 plot(vgmTlog, v.eye)
+fitLine.df  <- variogramLine(v.eye, maxdist = 60000)
+summary(v.eye)
+# plot.new()
+# plot(vgmTlog, v.eye, col = "red", lwd = 3, cex.lab=1.2,
+#      xlab = 'Distance [m]', xlim = c(0,40000),
+#      ylab = expression("Semivariance [ (ln"~degree*C~")"^2~"]"), ylim = c(0, 0.3)
+#      )
+vplot  <- ggplot() +
+        geom_line(data = fitLine.df, aes(x = dist, y = gamma), size = 1, color = "red") +
+        geom_point(data = vgmTlog, aes(x = dist, y = gamma), color = "blue")
+vplot
+#geom_text(aes(label=np))
+vhline  <- vplot + geom_hline(yintercept = c(0.155, 0.44), linetype = 2, color = "green") +
+        geom_vline(xintercept = c(700, 35000), linetype = 4, color = "orange" )
+limitsX  <- c(0,45000)
+breaksX  <- seq(limitsX[1], limitsX[2], 5000)
+labelsX=c(breaksX/1000)
+##limitsY  <- c(41,47)
+limitsY  <- c(0,0.5)
+breaksY  <- seq(limitsY[1],limitsY[2], 0.05)
+labelsY=as.character(breaksY)
+
+vbase  <- vhline +
+        xlab("Distance [km]") +
+        scale_x_continuous(breaks=breaksX,
+                           labels=labelsX,
+                           limits=limitsX) +
+        ylab(expression("Semivariance [ (ln"~degree*C~")"^2~"]")) +
+        scale_y_continuous(breaks=breaksY,
+                           labels=labelsY,
+                           limits=limitsY)
+
+vtext  <-
+        vbase +
+        geom_text(aes(x = 30000, y = 0.29), label = "sill = 0.155", angle = 0,
+                  family = "Times") +
+        geom_text(aes(x = 6000, y = 0.14), label = "sill = 0.125", angle = 0,
+                  family = "Times") +
+        geom_text(aes(x = 44000, y = 0.08), label = "Gaussian model \n (Vertical)", angle = 90,
+                  family = "Times") +
+        geom_text(aes(x = 44000, y = 0.21), label = "Spherical model \n (Lateral)", angle = 90,
+                  family = "Times") +
+        geom_text(aes(x = 6000, y = 0.02), label = "Range = 700 m", family = "Times") +
+        geom_text(aes(x = 30000, y = 0.18), label = "Range = 35 km", family = "Times")
+# sd  <- sd(vgmTlog$gamma)
+# vtext + geom_ribbon(data = vgmTlog, aes(x = dist, ymin=gamma -2*sd, ymax=gamma+2*sd),alpha=1)
+jp3dVariogram  <- vtext +
+        theme_bw(base_size = 12, base_family = "Times") +
+        theme(axis.title.x=element_text(vjust = -0.5))
+jp3dVariogram
+# ggsave(plot jp3dVariogram, "hkdVariogram.pdf", width = 7, height = 5)
+# # #ge.ggsave(hkdVariogram)
+# getwd()
+# ## grid$TlogUK.eye <- krige(Tlog~z, spdf, grid, model = v.eye)
+# ##TlogUK <- krige(Tlog~z, spdf, grid, model = v.eye, nmin =2, nmax = 6)
+# TlogUK <- krige(Tlog~1, spdf, grid, model = v.eye, nmin =6, nmax = 12)
+# TlogUK
+# grid.df  <- as.data.frame(TlogUK)
+# grid.df$Tpred0  <- exp(grid.df$var1.pred)
+# grid.df$Tpred1  <- exp(grid.df$var1.pred + 0.5*grid.df$var1.var)
+# head(grid.df)
+# hist(grid.df$var1.pred)
+# summary(grid.df)
+# ggplot(grid.df) +
+#         geom_raster(aes(x = x, y =y, fill = Tpred1)) +
+#         facet_wrap(~z)
+
 
 # fit.variogram(vgmTlog,v.eye)
 ### grid
@@ -78,113 +145,113 @@ plot(vgmTlog, v.eye)
 
 
 
-head(grid)
-### log plot
-vgmTlog <- variogram(tlog ~ 1, spdf, cutoff = 100000)
-vgmTlog
-vgmTlog <- variogram(Tlog ~ 1, spdf,
-                     boundaries = c(seq(100,1000,100), seq(2000,10000,1000)))
-vgmTlog <- variogram(Tlog ~ z, spdf,
-                     boundaries = c(seq(100,1000,100), seq(2000,45000,5000)))
-plot(vgmTlog)
-## Universal Kriging
-#vgmTlog <- variogram(Tlog ~ z^3, spdf, cutoff = 50000,cloud = T)
-#vgmTlog <- variogram(logT ~ z, spdf, cutoff = 2500, alpha = c(0,45,90,135), beta = c(0,45,90,135))
-# Trend
-#trend  <- gstat(NULL, "trend", logT ~ z^2, data = spdf)
-#vgmTlog  <- variogram(trend, cutoff = 2500)
-show.vgms()
-
-
-v.eye  <- vgm(0.28, "Pen", 35000, 0)
-v.eye1  <- vgm(psill = 0.16,  model = "Gau",  range=700,  nugget=0)
-v.eye   <- vgm(psill = 0.13,  model = "Sph",  range=35000,  nugget=0,  add.to=v.eye1)
-v.eye
-plot(vgmTlog, v.eye)
-
-vgmGauTlog <- fit.variogram(vgmTlog, v.eye)
-vgmGauTlog
-plot(vgmTlog, vgmGauTlog)
-plot(vgmTlog, v.eye)
-attr(vgmGauTlog, "SSErr")
-grid$TlogUK.eye <- krige(Tlog~z, spdf, grid, model = v.eye)
-show.vgms
-
-### post-krige
-log(exp(3))
-exp(1)
-uk.df  <- as.data.frame(Tlog_uk)
-uk.df$Tuk  <- 10^uk.df$var1.pred
-uk.df$TukCol  <- as.factor(round(uk.df$Tuk ))
-head(uk.df)
-summary(uk.df)
-#ge.df2csv(uk.df)
-#### 3D
-if(require(scatterplot3d)){
-        scatterplot3d(uk.df[,c(1,2,3)])
-}
-
-
-getwd()
-library(rgl)
-
-
-
-
-
-str(Tlog_uk)
-
-vgmSphTlog <- fit.variogram(vgmTlog, vgm(0.08, "Sph", 2500, 0))
-vgmSphTlog
-plot(vgmTlog, vgmSphTlog)
-attr(vgmSphTlog, "SSErr")
-
-vgmExpTlog <- fit.variogram(vgmTlog, vgm(0.08, "Exp", 2500, 0))
-vgmExpTlog
-plot(vgmTlog, vgmExpTlog)
-attr(vgmExpTlog, "SSErr")
-vgmLinTlog <- fit.variogram(vgmTlog, vgm(0.08, "Lin", 2500, 0))
-vgmLinTlog
-plot(vgmTlog, vgmLinTlog)
-attr(vgmLinTlog, "SSErr")
-vgmMatTlog <- fit.variogram(vgmTlog, vgm(0.08, "Mat", 2500, 0, kappa=3))
-vgmMatTlog
-plot(vgmTlog, vgmMatTlog)
-attr(vgmMatTlog, "SSErr")
-
-
-vgmTlogok  <- fit.variogram(vgmTlog, vgmSphTlog)
-vgmTlogok
-plot(vgmTlog, vgmTlogok)
-
-
-
-## 3D IDW
-
-head(vgm())
-
-## 3D variogram
-vgmT <- variogram(t ~ x+y+z, spdf, cutoff = 2500)
-vgmT
-plot(vgmT)
-
-vgmSphT <- fit.variogram(vgmT, vgm(3000, "Sph", 2500, 0))
-vgmSphT
-plot(vgmT, vgmSphT)
-attr(vgmSphT, "SSErr")
-vgmExpT <- fit.variogram(vgmT, vgm(3000, "Exp", 2500, 0))
-vgmExpT
-plot(vgmT, vgmExpT)
-attr(vgmExpT, "SSErr")
-vgmLinT <- fit.variogram(vgmT, vgm(3000, "Lin", 2500, 0))
-vgmLinT
-plot(vgmT, vgmLinT)
-attr(vgmLinT, "SSErr")
-vgmMatT <- fit.variogram(vgmT, vgm(3000, "Mat", 2500, 0, kappa=3))
-vgmMatT
-plot(vgmT, vgmMatT)
-attr(vgmMatT, "SSErr")
-
-vgmTok  <- fit.variogram(vgmT, vgmSphT)
-vgmTok
-plot(vgmT, vgmTok)
+# head(grid)
+# ### log plot
+# vgmTlog <- variogram(tlog ~ 1, spdf, cutoff = 100000)
+# vgmTlog
+# vgmTlog <- variogram(Tlog ~ 1, spdf,
+#                      boundaries = c(seq(100,1000,100), seq(2000,10000,1000)))
+# vgmTlog <- variogram(Tlog ~ z, spdf,
+#                      boundaries = c(seq(100,1000,100), seq(2000,45000,5000)))
+# plot(vgmTlog)
+# ## Universal Kriging
+# #vgmTlog <- variogram(Tlog ~ z^3, spdf, cutoff = 50000,cloud = T)
+# #vgmTlog <- variogram(logT ~ z, spdf, cutoff = 2500, alpha = c(0,45,90,135), beta = c(0,45,90,135))
+# # Trend
+# #trend  <- gstat(NULL, "trend", logT ~ z^2, data = spdf)
+# #vgmTlog  <- variogram(trend, cutoff = 2500)
+# show.vgms()
+#
+#
+# v.eye  <- vgm(0.28, "Pen", 35000, 0)
+# v.eye1  <- vgm(psill = 0.16,  model = "Gau",  range=700,  nugget=0)
+# v.eye   <- vgm(psill = 0.13,  model = "Sph",  range=35000,  nugget=0,  add.to=v.eye1)
+# v.eye
+# plot(vgmTlog, v.eye)
+#
+# vgmGauTlog <- fit.variogram(vgmTlog, v.eye)
+# vgmGauTlog
+# plot(vgmTlog, vgmGauTlog)
+# plot(vgmTlog, v.eye)
+# attr(vgmGauTlog, "SSErr")
+# grid$TlogUK.eye <- krige(Tlog~z, spdf, grid, model = v.eye)
+# show.vgms
+#
+# ### post-krige
+# log(exp(3))
+# exp(1)
+# uk.df  <- as.data.frame(Tlog_uk)
+# uk.df$Tuk  <- 10^uk.df$var1.pred
+# uk.df$TukCol  <- as.factor(round(uk.df$Tuk ))
+# head(uk.df)
+# summary(uk.df)
+# #ge.df2csv(uk.df)
+# #### 3D
+# if(require(scatterplot3d)){
+#         scatterplot3d(uk.df[,c(1,2,3)])
+# }
+#
+#
+# getwd()
+# library(rgl)
+#
+#
+#
+#
+#
+# str(Tlog_uk)
+#
+# vgmSphTlog <- fit.variogram(vgmTlog, vgm(0.08, "Sph", 2500, 0))
+# vgmSphTlog
+# plot(vgmTlog, vgmSphTlog)
+# attr(vgmSphTlog, "SSErr")
+#
+# vgmExpTlog <- fit.variogram(vgmTlog, vgm(0.08, "Exp", 2500, 0))
+# vgmExpTlog
+# plot(vgmTlog, vgmExpTlog)
+# attr(vgmExpTlog, "SSErr")
+# vgmLinTlog <- fit.variogram(vgmTlog, vgm(0.08, "Lin", 2500, 0))
+# vgmLinTlog
+# plot(vgmTlog, vgmLinTlog)
+# attr(vgmLinTlog, "SSErr")
+# vgmMatTlog <- fit.variogram(vgmTlog, vgm(0.08, "Mat", 2500, 0, kappa=3))
+# vgmMatTlog
+# plot(vgmTlog, vgmMatTlog)
+# attr(vgmMatTlog, "SSErr")
+#
+#
+# vgmTlogok  <- fit.variogram(vgmTlog, vgmSphTlog)
+# vgmTlogok
+# plot(vgmTlog, vgmTlogok)
+#
+#
+#
+# ## 3D IDW
+#
+# head(vgm())
+#
+# ## 3D variogram
+# vgmT <- variogram(t ~ x+y+z, spdf, cutoff = 2500)
+# vgmT
+# plot(vgmT)
+#
+# vgmSphT <- fit.variogram(vgmT, vgm(3000, "Sph", 2500, 0))
+# vgmSphT
+# plot(vgmT, vgmSphT)
+# attr(vgmSphT, "SSErr")
+# vgmExpT <- fit.variogram(vgmT, vgm(3000, "Exp", 2500, 0))
+# vgmExpT
+# plot(vgmT, vgmExpT)
+# attr(vgmExpT, "SSErr")
+# vgmLinT <- fit.variogram(vgmT, vgm(3000, "Lin", 2500, 0))
+# vgmLinT
+# plot(vgmT, vgmLinT)
+# attr(vgmLinT, "SSErr")
+# vgmMatT <- fit.variogram(vgmT, vgm(3000, "Mat", 2500, 0, kappa=3))
+# vgmMatT
+# plot(vgmT, vgmMatT)
+# attr(vgmMatT, "SSErr")
+#
+# vgmTok  <- fit.variogram(vgmT, vgmSphT)
+# vgmTok
+# plot(vgmT, vgmTok)
